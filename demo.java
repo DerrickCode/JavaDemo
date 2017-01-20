2a2ee8bc22f4327c01f60cb4b28cee68d4841f49
Derrick Wang <derrickwang12138@gmail.com>
2017-01-20 23:18:48 +0800

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Derrick Wang on 2017/1/20.
+ */
+public class demo {
+    public static void main(String[] args) {//psvm
+        System.out.println("welcome to Java Programming");//sout
+        System.out.printf("%s\n%s\n","welcome to","Java Programming!");
+    }
+}
